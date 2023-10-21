6b258b66d4bd70bc38513354e6b099c6be5d15d7
OmarBarba <84804799+OmarBarba@users.noreply.github.com>
2023-10-20 21:38:08 -0600

    Aprendizaje Probabilistico

--- 04_Aprendizaje no supervisado.py
+++ 04_Aprendizaje no supervisado.py
@@ -29,3 +29,68 @@ plt.scatter(centroids[:, 0], centroids[:, 1], c='black', marker='X', s=200, labe
 plt.title('Agrupamiento K-Means')
 plt.legend()
 plt.show()
+
+
+#########################################################################################################
+#########################################SIN LIBRERIAS###################################################
+#########################################################################################################
+
+import numpy as np
+import random
+import matplotlib.pyplot as plt
+
+# Datos de ejemplo
+data = np.array([[2, 3], [2, 5], [8, 1], [5, 8], [7, 2], [6, 6]])
+
+# Número de grupos (clusters) deseados
+k = 2
+
+# Inicialización aleatoria de centroides
+centroids = [data[i] for i in random.sample(range(len(data)), k)]
+
+# Número máximo de iteraciones
+max_iter = 100
+
+for iter in range(max_iter):
+    # Inicializar clusters vacíos
+    clusters = [[] for _ in range(k)]
+
+    # Asignar cada punto de datos al cluster más cercano
+    for point in data:
+        distances = [np.linalg.norm(point - centroid) for centroid in centroids]
+        cluster_id = np.argmin(distances)
+        clusters[cluster_id].append(point)
+
+    # Calcular nuevos centroides como el promedio de los puntos en cada cluster
+    new_centroids = [np.mean(cluster, axis=0) for cluster in clusters]
+
+    # Comprobar si los centroides han convergido
+    if np.array_equal(centroids, new_centroids):
+        break
+
+    # Actualizar centroides
+    centroids = new_centroids
+
+print("Centroides finales:")
+for i, centroid in enumerate(centroids):
+    print(f"Cluster {i + 1}: {centroid}")
+
+print("Puntos por cluster:")
+for i, cluster in enumerate(clusters):
+    print(f"Cluster {i + 1}: {cluster}")
+
+# Graficar los resultados
+colors = ['b', 'g', 'r', 'c', 'm', 'y']
+for i, cluster in enumerate(clusters):
+    points = np.array(cluster)
+    plt.scatter(points[:, 0], points[:, 1], c=colors[i], label=f'Cluster {i + 1}')
+
+centroid_points = np.array(centroids)
+plt.scatter(centroid_points[:, 0], centroid_points[:, 1], marker='x', s=100, c='k', label='Centroides')
+
+plt.xlabel('Eje X')
+plt.ylabel('Eje Y')
+plt.title('Resultados del Agrupamiento K-Means')
+plt.legend()
+plt.grid(True)
+plt.show()
